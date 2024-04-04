4d27b861990659cb8e56acb2ab29de8ff633dc7c
Jihad580 <jihadhasan0580@gmail.com>
2024-04-04 12:41:40 +0600

    math function

---
+++ math.py
@@ -0,0 +1,11 @@
+from math import *
+
+print (max(20,10))
+print (min(20,10))
+print (sqrt(20))
+print (pow(2,3))
+print (abs(-4))
+print (floor(20.47))
+print (ceil(20.57))
+print (round(20.44))
+print (round(20.66))
