00bda52b3249c6125d9013350b2c7669e432f6ec
Jihad580 <jihadhasan0580@gmail.com>
2024-04-04 13:37:01 +0600

    formatting

---
+++ formatted string.py
@@ -0,0 +1,24 @@
+'''
+type function
+
+'''
+print(type("Naim"))
+
+'''
+formating
+'''
+
+
+nub1 = 20
+nub2 = 10
+
+print (f"{nub1} + {nub2} = {nub1 + nub2}")
+
+
+print("Jihad hasan.",end=" ")
+print ("01621046299")
+
+'''
+new line 
+'''
+print ("Jihad \n hasan")
