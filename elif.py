5b568c07aa7b4272ade643ca38cdaa5a74aecb4d
Jihad580 <jihadhasan0580@gmail.com>
2024-05-14 16:58:31 +0600

    elif condition

---
+++ elif.py
@@ -0,0 +1,28 @@
+#elif condition
+mark = 71
+
+if mark>=80 :
+    print ("A+")
+
+elif mark>=70 :
+    print ("A")
+
+elif mark>= 60 :
+    print ("A-")
+
+elif mark>=50 :
+    print ("B")
+
+else :
+    print ("Fail")
+
+
+
+
+
+
+
+
+
+
+
